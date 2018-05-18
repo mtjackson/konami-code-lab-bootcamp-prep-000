5702fb4c318b9a93d716f1b9f15e4f5545154bb6
Mary Jackson <jackson.marytherese@gmail.com>
2018-05-18 00:24:56 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,24 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+  const input = document.querySelector('body')
+  let i = 0
+  input.addEventListener('keydown', function(e) {
+    function onKeyDownHandler(e) {
+      const key = e.key
+      var nextKey = codes[i]
+
+      if (key === nextKey) {
+        i++
+
+        if (i === codes.length) {
+          alert('hurray!')
+          i = 0
+        }
+      } else {
+        i = 0
+      }
+    }
+  })
+  
 }
